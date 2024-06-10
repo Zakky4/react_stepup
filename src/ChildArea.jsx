359a8d937ba28c0ed6f6f376f6e73cbb07fa1406
zakky <sadaoza@gmail.com>
2024-06-10 19:26:56 +0900

    Bad Scriptt Demo

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,18 @@
 const style = {
   width: "100%",
   height: "200px",
-  backgroundColor: "khaki"
+  backgroundColor: "khaki",
 };
 
 export const ChildArea = (props) => {
   const { open } = props;
+  console.log("ChildAreaがレンダリングされた!");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
       {open ? (

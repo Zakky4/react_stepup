309a56108b9512fe5719bd776a1f00f3648bac60
zakky <sadaoza@gmail.com>
2024-06-12 22:45:41 +0900

    Basic Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./App.css";
-import { CssModules } from "./components/CssModules";
-import { Emotion } from "./components/Emotion";
-import { InlineStyle } from "./components/InlineStyle";
-import { StyledJsx } from "./components/Styled";
-import { StyledComponents } from "./components/StyledComponents";
+import {
+  BrowserRouter,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 export default function App() {
   return (
-    <div className="App">
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }

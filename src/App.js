36bac9b2d11ad93c9fd2c02d58a86da2cd7d7a42
zakky <sadaoza@gmail.com>
2024-06-13 09:30:25 +0900

    Add RouterComp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom/cjs/react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page2DetailA";
+import { RouterComp } from "./router/Router";
 import "./App.css";
-import {
-  BrowserRouter,
-  Route,
-} from "react-router-dom/cjs/react-router-dom.min";
 
 export default function App() {
   return (
@@ -21,32 +13,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url }}) => (
-            <Switch>
-              {console.log(url)}
-              <Route exact path={ url }>
-                <Page1 />
-              </Route>
-              <Route path={`${ url }/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${ url }/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <RouterComp />
     </BrowserRouter>
   );
 }
